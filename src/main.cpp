8955b903edc7f4ad9c2a6dd646c5960e7fec0753
Huon <huonswales@gmail.com>
2024-03-17 17:11:49 +1000

    Impplemented alarm setting system

--- src/main.cpp
+++ src/main.cpp
@@ -21,13 +21,22 @@ void setup()
   pinMode(9, INPUT);   // Hour button
   pinMode(10, INPUT);  // Min button
   pinMode(12, INPUT);  // Function button
-  pinMode(13, OUTPUT); // Buzzer
+  pinMode(11, OUTPUT); // Buzzer
+  pinMode(13, INPUT);  // Disarm wire
 }
+
+bool settingAlarm = false;
+int alarm = -1;
 
 void loop()
 {
   checkButtons();
-  display.displayNumber(calculateTime());
+  if (calculateTime() == alarm && settingAlarm == false)
+    Serial.println("ALARM");
+  if (settingAlarm)
+    display.displayNumber(alarm < 0 ? 0 : alarm);
+  else
+    display.displayNumber(calculateTime());
 }
 
 // Time functions
@@ -57,15 +66,24 @@ int calculateTime()
 
 // Buttons functions
 
-bool hourBtnPress = false, minBtnPress = false;
+bool funcBtnPress = false, hourBtnPress = false, minBtnPress = false;
 
 void checkButtons()
 {
+  if (digitalRead(12) && !funcBtnPress)
+    settingAlarm = !settingAlarm;
   if (digitalRead(9) == HIGH && !hourBtnPress)
-    hour = hour >= 23 ? 0 : 1 + hour;
+    if (settingAlarm)
+      alarm = alarm / 100 >= 23 ? alarm - 2300 : (alarm == -1 ? 100 : 100 + alarm);
+    else
+      hour = hour >= 23 ? 0 : 1 + hour;
   if (digitalRead(10) == HIGH && !minBtnPress)
-    min = min >= 59 ? 0 : 1 + min;
+    if (settingAlarm)
+      alarm = alarm - ((alarm / 100) * 100) >= 59 ? (alarm / 100) * 100 : (alarm == -1 ? 1 : 1 + alarm);
+    else
+      min = min >= 59 ? 0 : 1 + min;
 
   hourBtnPress = digitalRead(9) == LOW ? false : true;
   minBtnPress = digitalRead(10) == LOW ? false : true;
+  funcBtnPress = digitalRead(12) == LOW ? false : true;
 }
