37cf7ff501b43241a8707464eb362d8069e10197
Huon <huonswales@gmail.com>
2024-03-17 16:20:29 +1000

    Reoragnised the main loop into functions

--- src/main.cpp
+++ src/main.cpp
@@ -6,54 +6,40 @@
 // sudo chmod a+rw /dev/ttyACM0
 
 void checkButtons();
+int calculateTime();
 
 SevenSegDisplay display;
 
 void setup()
 {
+  display.useShiftRegister(true);
   Serial.begin(9600);
   while (!Serial)
   {
-    // wait for serial conn
   }
-  display.useShiftRegister(true);
 
-  pinMode(9, INPUT);   // button
-  pinMode(10, INPUT);  // button
-  pinMode(11, OUTPUT); // buzzer
+  pinMode(9, INPUT);   // Hour button
+  pinMode(10, INPUT);  // Min button
+  pinMode(12, INPUT);  // Function button
+  pinMode(13, OUTPUT); // Buzzer
 }
-
-int hour = 12, min = 0;
-unsigned long currentMillis, previousMillis = 0;
-const long interval = 60000;
-bool hourBtnPress = false, minBtnPress = false;
 
 void loop()
 {
+  checkButtons();
+  display.displayNumber(calculateTime());
+}
 
-  if (digitalRead(9) == HIGH && !hourBtnPress)
-  {
-    hourBtnPress = true;
-    if (hour >= 23)
-      hour = 0;
-    else
-      hour++;
-  }
-  if (digitalRead(10) == HIGH && !minBtnPress)
-  {
-    minBtnPress = true;
-    if (min >= 59)
-      min = 0;
-    else
-      min++;
-  }
-  if (digitalRead(9) == LOW)
-    hourBtnPress = false;
-  if (digitalRead(10) == LOW)
-    minBtnPress = false;
+// Time functions
+
+int hour = 12, min = 0;
+unsigned long currentMillis, previousMillis = 0;
+
+int calculateTime()
+{
   currentMillis = millis();
 
-  if (currentMillis - previousMillis >= interval)
+  if (currentMillis - previousMillis >= 60000)
   {
     // save the last time you updated the time
     previousMillis = currentMillis;
@@ -66,6 +52,20 @@ void loop()
       hour = hour >= 23 ? 0 : 1 + hour;
     }
   }
+  return min + (hour * 100);
+}
 
-  display.displayNumber(min + (hour * 100));
+// Buttons functions
+
+bool hourBtnPress = false, minBtnPress = false;
+
+void checkButtons()
+{
+  if (digitalRead(9) == HIGH && !hourBtnPress)
+    hour = hour >= 23 ? 0 : 1 + hour;
+  if (digitalRead(10) == HIGH && !minBtnPress)
+    min = min >= 59 ? 0 : 1 + min;
+
+  hourBtnPress = digitalRead(9) == LOW ? false : true;
+  minBtnPress = digitalRead(10) == LOW ? false : true;
 }
